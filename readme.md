3834c96c5825eb94f9e52fea2edbd12dfb46289f
futao <1185172056@qq.com>
2020-04-29 13:58:19 +0800

    nacos-config

--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
+### # Nacos
 * 启动nacos-server `sh startup.sh -m standalone`
 * 停止nacos-server `sh shutdown.sh`
--- service-user/src/main/java/com/futao/springcloud/demo/alibaba/service/user/UserApplication.java
+++ service-user/src/main/java/com/futao/springcloud/demo/alibaba/service/user/UserApplication.java
@@ -1,18 +1,33 @@
 package com.futao.springcloud.demo.alibaba.service.user;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
  * @author futao
  * @date 2020/4/28
  */
-
+@Slf4j
 @SpringBootApplication
 @EnableDiscoveryClient
 public class UserApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class);
+        ConfigurableApplicationContext context = SpringApplication.run(UserApplication.class);
+
+        new Thread(() -> {
+            ConfigurableEnvironment environment = context.getEnvironment();
+            while (true) {
+                log.info("{}", environment.getProperty("spring.application.name"));
+                try {
+                    Thread.sleep(300L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
